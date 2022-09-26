4725d2e6c00fc95c870abcb172d8ce038ab969a5
sss <sshoony95@gmail.com>
2022-09-26 17:57:28 -0400

    Fix error

--- src/components/ProfileIcon.js
+++ src/components/ProfileIcon.js
@@ -19,7 +19,7 @@ const ProfileIcon = (props) => {
       <path stroke={props.color} stroke-width='3' stroke-linecap='butt'  stroke-linejoin='miter' stroke-miterlimit= '4' fill= 'none' opacity= '1' vector-effect="non-scaling-stroke"  transform=" translate(0, 0)" d="M 22.96723 -39.78052 L 45.93458 0 L 22.967229999999997 39.78052 L -22.96724 39.78052 L -45.93459 0 L -22.96724 -39.78052 z" />
       </g>
       <g transform="matrix(6.05 0 0 5.15 156.58 273.73)" id="Jh_ccYGs_vW8IhJ4hBmwV"  >
-      <path stroke={props.color} stroke-width='3' stroke-linecap='butt'  stroke-linejoin='miter' stroke-miterlimit= '4' fill= 'none' opacity= '1' vector-effect="non-scaling-stroke"  transform=" translate(0, 0)" d="M 13.08073 -14.8495 L 25.57281 -0.40273000000000003 L 25.57281 14.8495 L -25.57281 14.8495 L -25.57281 -0.40273000000000003 L -12.646980000000001 -14.8495 z" stroke-linecap="round" />
+      <path stroke={props.color} stroke-width='3' stroke-linecap='butt'  stroke-linejoin='miter' stroke-miterlimit= '4' fill= 'none' opacity= '1' vector-effect="non-scaling-stroke"  transform=" translate(0, 0)" d="M 13.08073 -14.8495 L 25.57281 -0.40273000000000003 L 25.57281 14.8495 L -25.57281 14.8495 L -25.57281 -0.40273000000000003 L -12.646980000000001 -14.8495 z" />
       </g>
     </Svg>
      )
