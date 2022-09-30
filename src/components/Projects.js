d81cb9632f4744e50b0c1673842ade0ecb27bdb3
sss <sshoony95@gmail.com>
2022-09-30 10:41:48 -0400

    Add projects data

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -2,13 +2,16 @@ import styled from 'styled-components';
 import Project from './Project';
 import shopImage from '../images/shop.jpg';
 import waldoImage from '../images/where_is_waldo.jpg';
+import tictactoeImage from '../images/tic-tac-toe.jpg';
+import weatherAppImage from '../images/wheather-app.jpg';
+
 
 const Projects = (props) => {
-  const projectsList = [
+  const projectList = [
     {
       image: shopImage,
       name: 'Fake shop',
-      description: 'A simple fake store React app, built with React, React Router, Fake Store API, styled-components and Bootstrap.',
+      description: 'A fake store React app, built with React, React Router, Fake Store API, Styled components and Bootstrap.',
       github: 'https://github.com/shino022/Shop',
       link: 'https://shino022.github.io/Shop/'
     },
@@ -18,7 +21,22 @@ const Projects = (props) => {
       description: 'A Photo Tagging app built with React and Firebase.',
       github: 'https://github.com/shino022/Where-is-waldo',
       link: 'https://shino022.github.io/Where-is-waldo/'
+    },
+    {
+      image: weatherAppImage,
+      name: 'The Weather App',
+      description: 'A weather app built with Open Weather API, Giphy API, Webpack, HTML, CSS and Javascript.',
+      github: 'https://github.com/shino022/Weather-App',
+      link: 'https://shino022.github.io/Weather-App/'
+    },
+    {
+      image: tictactoeImage,
+      name: 'Tic Tac Toe',
+      description: 'A simple Tic-Tac-Toe game app built with HTML, CSS and Javascript.',
+      github: 'https://github.com/shino022/TIC-TAC-TOE',
+      link: 'https://shino022.github.io/TIC-TAC-TOE/'
     }
+
   ]
 
   const Section = styled.section`
@@ -32,8 +50,8 @@ const Projects = (props) => {
   return(
     <Section id='projects'>
       <props.Title>Projects</props.Title>
-      <Project colors={props.colors} mediaWidth={props.mediaWidth} project={projectsList[0]} reverse={false} />
-      <Project colors={props.colors} mediaWidth={props.mediaWidth} project={projectsList[1]} reverse={true} />
+      {projectList.map((data, index) =>
+        <Project colors={props.colors} mediaWidth={props.mediaWidth} project={data} reverse={index%2===1} key={data.name} />)}
     </Section>
 
   );
--- src/components/TechstackIcon.js
+++ src/components/TechstackIcon.js
@@ -22,7 +22,7 @@ const TechstackIcon = (props) => {
     width: 120px; 
     height: 120px;
     opacity: 0;
-    transition: opacity 1.5s ease-in-out ${(props.logo.order/10)+.5}s;
+    transition: opacity 1.5s ease-in-out ${(props.logo.order/10)}s;
     @media (max-width: ${props.mediaWidth.phone}) {
       width: 100px; 
       height: 100px;
